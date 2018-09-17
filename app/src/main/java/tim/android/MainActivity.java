07f83e0424caaa1801cd1d34feeacc24a48cf577
Programeezy <programeezy@gmail.com>
2018-09-17 13:23:10 +0300

    Add imei text on main activity

--- app/src/main/java/tim/android/MainActivity.java
+++ app/src/main/java/tim/android/MainActivity.java
@@ -1,7 +1,15 @@
 package tim.android;
 
-import android.support.v7.app.AppCompatActivity;
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.telephony.TelephonyManager;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +17,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TelephonyManager tm = (TelephonyManager)getSystemService(Context.TELEPHONY_SERVICE);
+        TextView imeiView = (TextView) findViewById(R.id.textView);
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE) !=
+                PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_PHONE_STATE)) {
+                Toast.makeText(this, "Give permissions", Toast.LENGTH_SHORT).show();
+            }
+
+            else {
+                ActivityCompat.requestPermissions(this, new String[] {
+                        Manifest.permission.READ_PHONE_STATE
+                }, 1);
+                imeiView.setText(tm.getDeviceId());
+            }
+        }
+
+        else {
+            imeiView.setText(tm.getDeviceId());
+        }
+        
+
     }
 }
