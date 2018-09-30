48e67f5637fac49e1cd8af92daac677c1aa1dcae
Programeezy <programeezy@gmail.com>
2018-09-30 22:25:25 +0300

    Add sync gradle with git

--- app/src/main/java/tim/android/MainActivity.java
+++ app/src/main/java/tim/android/MainActivity.java
@@ -4,6 +4,7 @@ import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -13,31 +14,56 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private final int REQUEST_PERMISSION_PHONE_STATE = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TelephonyManager tm = (TelephonyManager)getSystemService(Context.TELEPHONY_SERVICE);
+        TelephonyManager tm = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
         TextView imeiView = (TextView) findViewById(R.id.textView);
 
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE) !=
-                PackageManager.PERMISSION_GRANTED) {
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_PHONE_STATE)) {
-                Toast.makeText(this, "Give permissions", Toast.LENGTH_SHORT).show();
-            }
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.READ_PHONE_STATE)
+                != PackageManager.PERMISSION_GRANTED) {
 
-            else {
-                ActivityCompat.requestPermissions(this, new String[] {
-                        Manifest.permission.READ_PHONE_STATE
-                }, 1);
-                imeiView.setText(tm.getDeviceId());
+            // Permission is not granted
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.READ_PHONE_STATE)) {
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+            } else {
+                // No explanation needed; request the permission
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.READ_PHONE_STATE},
+                        REQUEST_PERMISSION_PHONE_STATE);
+
             }
+        } else {
+            imeiView.setText("Already");
         }
 
-        else {
-            imeiView.setText(tm.getDeviceId());
-        }
-        
+    }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        TextView imeiView = (TextView) findViewById(R.id.textView);
+
+        switch (requestCode) {
+            case REQUEST_PERMISSION_PHONE_STATE: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    imeiView.setText("Granted!");
+                } else {
+                    imeiView.setText("Denied!");
+                }
+                return;
+            }
+
+        }
     }
 }
