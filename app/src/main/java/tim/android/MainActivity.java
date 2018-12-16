5174585d5e41a7cd6c07a932e5480d78f0bdcbd3
Tim <programeezy@gmail.com>
2018-12-16 17:37:36 +0300

    Add profile fragment

--- app/src/main/java/tim/android/MainActivity.java
+++ app/src/main/java/tim/android/MainActivity.java
@@ -3,25 +3,65 @@ package tim.android;
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
-import android.support.v7.app.AppCompatActivity;
 import android.telephony.TelephonyManager;
+import android.view.MenuItem;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
+
+public class MainActivity extends AppCompatActivity implements ProfileFragment.OnFragmentInteractionListener {
 
     private final int REQUEST_PERMISSION_PHONE_STATE = 1;
+    private DrawerLayout mDrawerLayout;
+    private NavController navController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mDrawerLayout = findViewById(R.id.draw_layout);
+        navController = Navigation.findNavController(this, R.id.my_nav_host_fragment);
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        // set item as selected to persist highlight
+                        selectDrawerItem(menuItem);
+
+                        // Add code here to update the UI based on the item selected
+                        // For example, swap UI fragments here
+
+                        return true;
+                    }
+                });
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionbar = getSupportActionBar();
+        actionbar.setDisplayHomeAsUpEnabled(true);
+        actionbar.setHomeAsUpIndicator(R.drawable.ic_menu_black_24dp);
         TelephonyManager tm = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
-        TextView imeiView = (TextView) findViewById(R.id.textView);
+        TextView imeiView = findViewById(R.id.textView);
 
         if (ContextCompat.checkSelfPermission(this,
                 Manifest.permission.READ_PHONE_STATE)
@@ -46,6 +86,37 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void selectDrawerItem(MenuItem menuItem) {
+        // Создать новый фрагмент и задать фрагмент для отображения
+        // на основе нажатия на элемент навигации
+        Fragment fragment = null;
+        Class fragmentClass;
+        switch(menuItem.getItemId()) {
+            case R.id.nav_profile:
+                navController.navigate(R.id.profileFragment);
+                break;
+            default:
+                fragmentClass = ProfileFragment.class;
+        }
+
+        // Выделение существующего элемента выполнено с помощью
+        // NavigationView
+        menuItem.setChecked(true);
+        // Установить заголовок для action bar'а
+        setTitle(menuItem.getTitle());
+        // Закрыть navigation drawer
+        mDrawerLayout.closeDrawers();
+    }
+    @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         TextView imeiView = (TextView) findViewById(R.id.textView);
@@ -59,9 +130,14 @@ public class MainActivity extends AppCompatActivity {
                 } else {
                     imeiView.setText("Denied!");
                 }
-                return;
             }
 
         }
+    }
+
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
